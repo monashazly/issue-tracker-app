a0e3156f1ce0fcd79d179576489509cbaff2e0d3
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2023-11-03 22:56:40 +0200

    create an API for updating issue

--- app/api/issues/issue/route.ts
+++ app/api/issues/issue/route.ts
@@ -1,17 +1,17 @@
 import { NextRequest, NextResponse } from "next/server"
 import prisma from "@/prisma/client"
-import { createIssueSchema } from "../validationSchemas"
+import { issueSchema } from "../../validationSchemas"
 
 export async function POST(request : NextRequest) {
   const body = await request.json()
-  const validation = createIssueSchema.safeParse(body)
+  const validation = issueSchema.safeParse(body)
   if( !validation.success) return NextResponse.json(validation.error.format() , { status : 400})
 
   const createdIssue = await prisma.issue.create({
     data : {
       title : body.title,
-      description : body.description,
-      status : body.status
+      description : body.description
+      // status : body.status
     }
   });
   return NextResponse.json(createdIssue , { status : 201})
