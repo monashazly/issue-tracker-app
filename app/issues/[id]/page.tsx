065e1e58531eb9b5d34e57d039ad980f7e7caeba
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2023-11-05 14:06:53 +0200

    delete an issue

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,7 +3,7 @@ import prisma from "@/prisma/client"
 import {  Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes"
 import { notFound } from "next/navigation"
 import ReactMarkdown from "react-markdown"
-import { Pencil2Icon } from '@radix-ui/react-icons';
+import { Pencil2Icon} from '@radix-ui/react-icons';
 import Link from "next/link"
 import IssueStatusBadge from "@/app/components/IssueStatusBadge"
 import DeleteIssueButton from "../_components/DeleteButton";
--- app/issues/_components/DeleteButton.tsx
+++ app/issues/_components/DeleteButton.tsx
@@ -1,12 +1,26 @@
 'use client';
 
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import { TrashIcon } from "@radix-ui/react-icons";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const DeleteIssueButton = ({issueId} : {issueId : number}) => {
+
+    const router =  useRouter()
+    const deleteIssue = async()=>{
+        await axios.delete(`/api/issues/${issueId}`)
+        router.push('/issues')
+        router.refresh()
+    }
+
     return (
         <AlertDialog.Root>
             <AlertDialog.Trigger>
-                <Button color="red">Delete issue</Button>
+                <Button color="red">
+                    <TrashIcon/>
+                    Delete issue
+                </Button>
             </AlertDialog.Trigger>
             <AlertDialog.Content style={{ maxWidth: 450 }}>
                 <AlertDialog.Title>Delete Issue</AlertDialog.Title>
@@ -21,7 +35,7 @@ const DeleteIssueButton = ({issueId} : {issueId : number}) => {
                         </Button>
                     </AlertDialog.Cancel>
                     <AlertDialog.Action>
-                        <Button variant="solid" color="red">
+                        <Button variant="solid" color="red" onClick={deleteIssue}>
                             Delete
                         </Button>
                     </AlertDialog.Action>
