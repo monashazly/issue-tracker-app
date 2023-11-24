2bad1f54792a28751f58d4d7e8bead501fdd7ca5
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2023-11-24 21:42:38 +0200

    sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -9,60 +9,62 @@ import { ArrowUpIcon } from "@radix-ui/react-icons"
 import NextLink from 'next/link';
 
 
-interface Props{
-  searchParams : { status :Status , orderBy: keyof Issue }
+
+interface Props {
+  searchParams: { status: Status, orderBy: keyof Issue }
 }
 
 
 
 
-const IssuesPage = async ({ searchParams} :Props) => {
+const IssuesPage = async ({ searchParams }: Props) => {
 
   const statuses = Object.values(Status);
-
-  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
-
-  const issues = await prisma.issue.findMany({
-    where :{ status }
-  })
-
   const columns: {
     label: string;
     value: keyof Issue;
     className?: string;
   }[] = [{ label: "ID", value: "id" },
   { label: "Description", value: "description" },
-    { label: "Issue", value: "title" },
-    {
-      label: "Status",
-      value: "status",
-      className: "hidden md:table-cell",
-    },
-    {
-      label: "Created",
-      value: "createdAt",
-      className: "hidden md:table-cell",
-    },
-  ];
+  { label: "Issue", value: "title" },
+  {
+    label: "Status",
+    value: "status",
+    className: "hidden md:table-cell",
+  },
+  {
+    label: "Created",
+    value: "createdAt",
+    className: "hidden md:table-cell",
+  },
+    ];
+
+  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
+  const orderBy = columns.map(column => column.value).includes(searchParams.orderBy) ? { [searchParams.orderBy]: 'asc' } : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status },
+    orderBy
+  })
 
 
   return (
     <>
-        <Flex mb="5" justify="between">
-          <IssueStatusFilter />
-          <Button>
-            <Link href="/issues/new">New issue</Link>
-          </Button>
-        </Flex>
+      <Flex mb="5" justify="between">
+        <IssueStatusFilter />
+        <Button>
+          <Link href="/issues/new">New issue</Link>
+        </Button>
+      </Flex>
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
               <Table.ColumnHeaderCell key={column.value}>
                 <NextLink href={{
-                  query: { ...searchParams, orderBy: column.value }                  
+                  query: { ...searchParams, orderBy: column.value }
                 }}>{column.label}</NextLink>
-                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
+                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline" />}
               </Table.ColumnHeaderCell>
             ))}
           </Table.Row>
