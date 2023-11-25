ad6e508d8bedcb299531d18b27e77014e651a975
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2023-11-25 21:14:58 +0200

    fix navigator is not defined error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,5 @@
-import dynamic from "next/dynamic"
 
-const IssueForm = dynamic(()=> import ('@/app/issues/_components/IssueForm'))
+import IssueForm from "@/app/issues/_components/IssueForm"
 
 const NewIssuePage = () => {
   return (
