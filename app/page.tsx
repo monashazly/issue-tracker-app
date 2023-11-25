c86a272106f34eb2c87989ddf9efa1aadb1b31d1
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2023-11-25 18:18:45 +0200

    build chart with recharts

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,27 @@ import LatestIssues from "./LatestIssues"
 import { Flex } from "@radix-ui/themes"
 import IssuesSummary from "./IssuesSummary"
 import prisma from "@/prisma/client"
+import IssueChart from "./IssueChart"
 
 export default async function Home() {
-   
+
   const open = await prisma.issue.count({
-    where : {status : 'OPEN'}
+    where: { status: 'OPEN' }
   })
   const closed = await prisma.issue.count({
-    where : {status : 'CLOSED'}
+    where: { status: 'CLOSED' }
   })
   const inProgress = await prisma.issue.count({
-    where : {status : 'IN_PROGRESS'}
+    where: { status: 'IN_PROGRESS' }
   })
-     
+
   return (
-     <Flex  justify="between" gap="2">
-     <IssuesSummary  open={open} closed={closed} inProgress={inProgress}/>
-     <LatestIssues/>
-     </Flex>
+    <Flex justify="between" gap="2">
+      <Flex direction="column" gap="3">
+        <IssuesSummary open={open} closed={closed} inProgress={inProgress} />
+        <IssueChart open={open} closed={closed} inProgress={inProgress} />
+      </Flex>
+      <LatestIssues />
+    </Flex>
   )
 }
